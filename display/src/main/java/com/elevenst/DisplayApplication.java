4a7a24842fd2195a6ed29c9498a7ef24f24eb747
mincloud1501 <mincloud1501@naver.com>
2019-09-27 15:28:57 +0900

    Eureka Connection

--- display/src/main/java/com/elevenst/DisplayApplication.java
+++ display/src/main/java/com/elevenst/DisplayApplication.java
@@ -5,14 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrixDashboard
+
 public class DisplayApplication {
 
     @Bean
