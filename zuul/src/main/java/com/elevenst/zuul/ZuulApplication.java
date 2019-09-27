0fb49eb3eb5dc3dd82c55305f1e1c8be4ac1f6fb
mincloud1501 <mincloud1501@naver.com>
2019-09-27 16:05:41 +0900

    Eureka Connection

--- zuul/src/main/java/com/elevenst/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/elevenst/zuul/ZuulApplication.java
@@ -3,11 +3,14 @@ package com.elevenst.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableHystrixDashboard
+
 public class ZuulApplication {
 
     public static void main(String[] args) {
