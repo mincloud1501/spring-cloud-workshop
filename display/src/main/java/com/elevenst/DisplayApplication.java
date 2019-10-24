08de44b1abdf591cfcfef69f3d8d221204316edd
mincloud1501 <mincloud1501@naver.com>
2019-10-24 13:36:18 +0900

    swagger

--- display/src/main/java/com/elevenst/DisplayApplication.java
+++ display/src/main/java/com/elevenst/DisplayApplication.java
@@ -16,7 +16,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 @SpringBootApplication
 @EnableSwagger2
 @EnableCircuitBreaker
